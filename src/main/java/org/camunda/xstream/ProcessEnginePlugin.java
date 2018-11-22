540cc247ffe98eb738fe9d7c397447da42d0d987
Stephan Pelikan <stephan.pelikan@wdw-elab.de>
2018-11-22 12:34:10 +0100

    Solve issues 3,4

--- src/main/java/org/camunda/xstream/ProcessEnginePlugin.java
+++ src/main/java/org/camunda/xstream/ProcessEnginePlugin.java
@@ -12,15 +12,30 @@ public class ProcessEnginePlugin extends AbstractProcessEnginePlugin {
 
     private String encoding = "UTF-8";
 
+    private List<String> converters = new LinkedList<String>();
+
+    private List<String> allowedTypes = new LinkedList<String>();
+
     @SuppressWarnings("rawtypes")
     @Override
     public void preInit(final ProcessEngineConfigurationImpl processEngineConfiguration) {
+        if ((allowedTypes == null) || allowedTypes.isEmpty()
+        		|| ((allowedTypes.size() == 1) && allowedTypes.get(0).isEmpty())) {
+        	throw new RuntimeException("No or empty 'allowedTypes' parameter set!\n"
+        			+ "Since XStream 1.4.10 (used by camunda-xstream) security rules "
+        			+ "have to be declared, by explicitly naming classes allowed to be deserialized.\n"
+        			+ "Have a look at https://github.com/RasPelikan/camunda-xstream/blob/master/README.md "
+        			+ "how to do so.");
+        }
+    	
         final List<TypedValueSerializer> customPreVariableSerializers = processEngineConfiguration.getCustomPreVariableSerializers();
         final List<TypedValueSerializer> newPreVariableSerializers = new LinkedList<TypedValueSerializer>();
         if (customPreVariableSerializers != null) {
             newPreVariableSerializers.addAll(customPreVariableSerializers);
         }
-        newPreVariableSerializers.add(new XStreamObjectSerializer(encoding));
+        newPreVariableSerializers.add(
+        		new XStreamObjectSerializer(
+        				encoding, converters, allowedTypes));
         processEngineConfiguration.setCustomPreVariableSerializers(newPreVariableSerializers);
     }
 
@@ -31,9 +46,29 @@ public class ProcessEnginePlugin extends AbstractProcessEnginePlugin {
             this.encoding = null;
         }
     }
-
-    public String getEncoding() {
-        return encoding;
+    
+    // Used by wildfly plugin configuration
+    public void setAllowedTypes(String allowedTypes) {
+    	if (allowedTypes == null) {
+    		this.allowedTypes = null;
+    		return;
+    	}
+    	final String[] allowedTypesArray = allowedTypes.split(",");
+    	for (int i = 0; i < allowedTypesArray.length; ++i) {
+    		this.allowedTypes.add(allowedTypesArray[i]);
+    	}
+    }
+    
+    // Used by wildfly plugin configuration
+    public void setConverters(String converters) {
+    	if (converters == null) {
+    		this.converters = null;
+    		return;
+    	}
+    	final String[] convertersArray = converters.split(",");
+    	for (int i = 0; i < convertersArray.length; ++i) {
+    		this.converters.add(convertersArray[i]);
+    	}
     }
 
 }
